0a9628e6bcb9f43fe329f9297c4a684b1211508e
tholt1220 <trevholt1220@gmail.com>
2021-01-04 00:02:35 -0800

    updated git name

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:tholt1220/spotify.git master:gh-pages
+git push -f git@github.com:tholt1220/Spotify-Distance.git master:gh-pages
 cd -
