c93418de1d26eeffcf7acdd1586036cff29da5d0
tholt1220 <trevholt1220@gmail.com>
2021-01-04 00:43:19 -0800

    updated vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   lintOnSave: false
 }
+
+// vue.config.js file to be place in the root of your repository
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Spotify-Distance/'
+    : '/'
+}
